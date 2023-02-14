54c82067453b6faaa2197371fd2bb40c54034f10
YooSangbum <ysbjjyes@gmail.com>
2023-02-14 20:36:19 +0900

    23.02.14(야간자습)

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ var swiper = new Swiper('.reviewSwiper', {
     nextEl: '.next1',
     prevEl: '.prev1',
   },
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   grabCursor: true,
+  breakpoints: {
+  
+    768: {
+      slidesPerView:2 ,
+    },
+    1024: {
+      slidesPerView:3,
+    }
+  }
 });
