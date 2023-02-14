77d4052092230a8c5295677a38cb2ef6e1de1f3b
YooSangbum <ysbjjyes@gmail.com>
2023-02-14 17:55:52 +0900

    23.02.14(화)

---
+++ index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.reviewSwiper', {
+  navigation: {
+    nextEl: '.next1',
+    prevEl: '.prev1',
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  grabCursor: true,
+});
