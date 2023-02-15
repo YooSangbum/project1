be241b46e419c36926b0241fcdee5ddddbd512ca
YooSangbum <ysbjjyes@gmail.com>
2023-02-15 18:04:07 +0900

    23.02.15

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.reviewSwiper', {
+let swiper = new Swiper('.reviewSwiper', {
   navigation: {
     nextEl: '.next1',
     prevEl: '.prev1',
@@ -8,12 +8,26 @@ var swiper = new Swiper('.reviewSwiper', {
   loop: true,
   grabCursor: true,
   breakpoints: {
-  
     768: {
-      slidesPerView:2 ,
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView:3,
-    }
-  }
+      slidesPerView: 3,
+    },
+  },
+});
+
+// $('.btn_gotop').click(function () {
+//   $('html, body').animate({ scrollTop: 0 }, 400);
+//   return false;
+// });
+
+let swiper2 = new Swiper('.pdSwiper', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  grabCursor: true,
 });
